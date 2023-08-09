adf1b25dfbe8b978c1620bf9153ca9e83300fb1b
Lucas <lucasalexkuhn@gmail.com>
2023-08-09 16:58:06 -0300

    first commit

---
+++ src/PasswordGenerator.svelte
@@ -0,0 +1,66 @@
+<script>
+	import unidecode from 'unidecode';
+	import { onMount } from 'svelte';
+
+	let words = [];
+	let password = ' ';
+	let isCopied = false;
+
+	onMount(() => {
+		fetch('/words.txt')
+			.then((response) => response.text())
+			.then((text) => {
+				words.push(...text.split('\n'));
+			});
+	});
+
+	function getRandomWord() {
+		return words[Math.floor(Math.random() * words.length)];
+	}
+
+	async function generatePassword() {
+		const button = this;
+
+		button.setAttribute('aria-busy', 'true');
+
+		await new Promise((r) => setTimeout(r, 300));
+
+		let selected = [getRandomWord(), getRandomWord(), getRandomWord()];
+		selected = selected.map((word) => unidecode(word));
+		password = selected.join('-');
+
+		button.removeAttribute('aria-busy');
+	}
+
+	async function copyPasswordToClipboard() {
+		isCopied = true;
+		setTimeout(() => {
+			isCopied = false;
+		}, 150);
+		// Get current password from whatever value is on the field, in case the user changed it manually
+		password = document.querySelector('input').value;
+		navigator.clipboard.writeText(password);
+	}
+</script>
+
+<article>
+	<h1>Gerador de senha memorável</h1>
+	<div class="grid">
+		<input type="text" disabled={isCopied} value={password}/>
+		<button class="secondary" on:click={copyPasswordToClipboard}>copiar</button>
+	</div>
+	<button on:click={generatePassword}> Gerar </button>
+</article>
+
+<style>
+	input[type='text'] {
+		text-align: center;
+	}
+
+	.grid {
+		display: grid;
+		grid-template-columns: 1fr auto;
+		grid-gap: 10px;
+		align-items: center;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import PasswordGenerator from '../PasswordGenerator.svelte';
+</script>
+
+<main class="container">
+    <div style="width: 100%;">
+	    <PasswordGenerator />
+    </div>
+</main>
+
+<style>
+	main {
+		align-items: center;
+		height: 100vh;
+	}
+</style>
