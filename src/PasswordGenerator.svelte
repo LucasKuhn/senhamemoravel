66ed43730834d4832ac62f17dc2e7867fa50e190
Lucas <lucasalexkuhn@gmail.com>
2023-10-05 12:19:00 -0300

    format and remove text while loading the button

--- src/PasswordGenerator.svelte
+++ src/PasswordGenerator.svelte
@@ -1,46 +1,43 @@
 <script>
 	import { onMount } from 'svelte';
 	import unidecode from 'unidecode';
-	import { animate, stagger } from 'motion'
+	import { animate, stagger } from 'motion';
 
-	const name = 'Senha memorável'
-	let button = null
-	let words = []
-	let password = ' '
-	let isCopied = false
-	let shouldCapitalize = false
-	let shouldAddNumber = false
-	let letters = []
+	const name = 'Senha memorável';
+	let button = null;
+	let words = [];
+	let password = ' ';
+	let isCopied = false;
+	let shouldCapitalize = false;
+	let shouldAddNumber = false;
+	let letters = [];
 	let animation = null;
-	
+
 	onMount(() => {
 		fetch('/words.txt')
 			.then((response) => response.text())
 			.then((text) => {
 				words.push(...text.split('\n'));
 			});
-		// After loading the page, simulate a click on the button to generate a password
 		animation = animate(
 			(progress) => {
-				console.log('progress', progress)
+				console.log('progress', progress);
 
 				// The innter html of each letter should be a random letter from the alphabet
 				letters.forEach((letter) => {
-					letter.innerHTML = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-				})
+					letter.innerHTML = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+				});
 				// And when the animation is done, it should be the original letter
 				if (progress === 1) {
 					letters.forEach((letter, i) => {
-						letter.innerHTML = name.split('')[i]
-					})
+						letter.innerHTML = name.split('')[i];
+					});
 				}
 			},
 			{ duration: Infinity, easing: 'ease-out' }
 		);
 		generatePassword();
 	});
-
-
 
 	function getRandomWordFromList() {
 		return words[Math.floor(Math.random() * words.length)];
@@ -50,14 +47,16 @@
 		let selectedWord = getRandomWordFromList();
 		if (selectedWord.length <= 7) {
 			return selectedWord;
-		}
-		else {
+		} else {
 			return getRandomWord();
 		}
 	}
 
 	async function generatePassword() {
+		// Store the original text in a variable
+		const originalText = button.innerHTML;
 		button.setAttribute('aria-busy', 'true');
+		button.innerHTML = '';
 		animation.play();
 		password = ' ';
 		await new Promise((r) => setTimeout(r, 400));
@@ -66,11 +65,12 @@
 		password = selected.join('-');
 		capitalize();
 		addNumber();
+		button.innerHTML = originalText;
 		button.removeAttribute('aria-busy');
 		button.blur();
 		animation.finish();
 	}
-	
+
 	function capitalize() {
 		if (shouldCapitalize) {
 			password = password
@@ -110,10 +110,10 @@
 <article>
 	<h1>Gerador de senha</h1>
 	<h2 class="letters">
-	{#each name.split('') as letter, i}
-		<span bind:this={letters[i]}></span>
-	{/each}
-	&nbsp;
+		{#each name.split('') as letter, i}
+			<span bind:this={letters[i]} />
+		{/each}
+		&nbsp;
 	</h2>
 
 	<div class="mygrid">
